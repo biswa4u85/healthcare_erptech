fc2371d81f146269497319ca76e48c2dbdfffa21
biswa4u85 <biswa4u85@gmail.com>
2023-09-12 22:13:28 +0200

    add files

---
+++ healthcare_erptech/erptech_healthcare/doctype/hospital_settings/hospital_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, erptechin@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hospital Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
